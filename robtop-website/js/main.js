33593ea627cb27219672bc70a6b2b715d2f09620
veprogames <veprogames.gamedev@gmail.com>
2021-01-25 10:59:08 +0100

    format main.js

--- robtop-website/js/main.js
+++ robtop-website/js/main.js
@@ -1,48 +1,37 @@
-document.body.onload = function()
-{
+document.body.onload = function () {
     let menu = document.querySelector(".menu");
     let open_menu = document.getElementById("open_menu");
     let videoPlayer = document.querySelector(".video-player");
     let playButtons = document.querySelectorAll(".gamepreview button.play-trailer");
     let closeVideoPlayerButton = document.querySelector(".video-player button");
 
-    function closeMenu()
-    {
-        if(menu.classList.contains("shown"))
-        {
+    function closeMenu() {
+        if (menu.classList.contains("shown")) {
             menu.classList.remove("shown");
             menu.classList.add("hidden");
         }
     }
 
-    function openMenu()
-    {
-        if(menu.classList.contains("hidden"))
-        {
+    function openMenu() {
+        if (menu.classList.contains("hidden")) {
             menu.classList.remove("hidden");
             menu.classList.add("shown");
         }
     }
 
-    function openVideoPlayer(videoName)
-    {
-        if(!videoPlayer.classList.contains("active"))
-        {
+    function openVideoPlayer(videoName) {
+        if (!videoPlayer.classList.contains("active")) {
             videoPlayer.classList.add("active");
 
-            for(let iframe of videoPlayer.querySelectorAll("iframe"))
-            {
+            for (let iframe of videoPlayer.querySelectorAll("iframe")) {
                 iframe.style.display = iframe.dataset.video === videoName ? "block" : "none";
             }
         }
     }
 
-    function closeVideoPlayer()
-    {
-        if(videoPlayer.classList.contains("active"))
-        {
-            for(let iframe of videoPlayer.querySelectorAll("iframe"))
-            {
+    function closeVideoPlayer() {
+        if (videoPlayer.classList.contains("active")) {
+            for (let iframe of videoPlayer.querySelectorAll("iframe")) {
                 iframe.setAttribute("src", iframe.getAttribute("src"));
             }
 
@@ -50,15 +39,13 @@ document.body.onload = function()
         }
     }
 
-    menu.querySelectorAll("ul > li").forEach(item =>
-    {
+    menu.querySelectorAll("ul > li").forEach(item => {
         item.onclick = e => closeMenu();
     });
 
     open_menu.onclick = openMenu;
 
-    for(let button of playButtons)
-    {
+    for (let button of playButtons) {
         button.onclick = e => openVideoPlayer(button.dataset.video);
     }
 
@@ -70,26 +57,20 @@ document.body.onload = function()
 
     let animateTeasers = document.querySelectorAll(".game-teaser");
 
-    function checkFadeIns()
-    {
-        for(let el of animateDivs)
-        {
-            if(scrollY + innerHeight >= el.offsetTop)
-            {
+    function checkFadeIns() {
+        for (let el of animateDivs) {
+            if (scrollY + innerHeight >= el.offsetTop) {
                 el.classList.add("animate__animated", "animate__fadeIn");
             }
         }
 
-        for(let el of animateGames)
-        {
-            if(scrollY + innerHeight >= el.offsetTop)
-            {
+        for (let el of animateGames) {
+            if (scrollY + innerHeight >= el.offsetTop) {
                 let elements = Array.from(el.querySelectorAll("ul.features li"))
                     .concat(Array.from(el.querySelectorAll(".review")));
                 let character = el.querySelector(".character");
 
-                elements.forEach((li, idx) =>
-                {
+                elements.forEach((li, idx) => {
                     li.classList.add("animate__animated", "animate__zoomIn", "animate__faster");
                     li.style.animationDelay = (idx * 0.25) + "s";
                 });
@@ -98,10 +79,8 @@ document.body.onload = function()
             }
         }
 
-        for(let el of animateTeasers)
-        {
-            if(scrollY + innerHeight >= el.offsetTop)
-            {
+        for (let el of animateTeasers) {
+            if (scrollY + innerHeight >= el.offsetTop) {
                 let textTitle = el.querySelector("p.title");
                 let textSubTitle = el.querySelector("p.subtitle");
                 textTitle.classList.add("animate__animated", "animate__fadeInLeft");
