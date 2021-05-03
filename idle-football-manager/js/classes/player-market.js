2383d7a41b873f7f47c96e5fae957e7e8abc3467
veprogames <veprogames.gamedev@gmail.com>
2021-05-03 18:43:07 +0200

    update idle-football-manager/js/classes/player-market.js

--- idle-football-manager/js/classes/player-market.js
+++ idle-football-manager/js/classes/player-market.js
@@ -11,7 +11,7 @@ class PlayerMarket {
         let pow = Math.log(priceBase) / Math.log(statBase);
         for (let i = 0; i < Math.floor(Math.random() * 10) + 15; i++) {
             let fact = Math.min(10, 0.5 / Math.random()) + 0.5 * Math.random();
-            let price = Decimal.min(Decimal.max(new Decimal(1e2), game.money.div(10)), Decimal.pow(priceBase, GeneratorUtils.getNormRank(rank, game.country)).mul(250)).mul(1 + 3 * fact ** pow * (0.8 + 0.4 * Math.random()));
+            let price = Decimal.max(new Decimal(1e2), game.money.div(50)).mul(1 + 3 * fact ** pow * (0.8 + 0.4 * Math.random()));
             let minStat = Decimal.pow(price, Math.log(statBase) / Math.log(priceBase)).mul(2e-3);
             let maxStat = minStat.mul(1 + 0.5 * Math.random());
             let player = GeneratorUtils.generatePlayer(Math.floor(Math.random() * 1000000), minStat, maxStat, false, price);
